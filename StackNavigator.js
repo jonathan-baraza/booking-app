e468286892fd03191640bfbc56801b89480b9e2a
jonathan_baraza <jbaraza46@gmail.com>
2023-07-04 15:26:21 +0300

    navigators imports

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
   return (
     <View>
       <Text>StackNavigator</Text>
     </View>
-  )
-}
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
